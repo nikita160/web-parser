8f6701c94991f26f713433358b9679aad4011a52
Nikita <nikitax160@yandex.ru>
2022-06-10 17:25:17 +0300

    stat

---
+++ src/main/java/com/nikitax/webparser/WebParserApplication.java
@@ -0,0 +1,13 @@
+package com.nikitax.webparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebParserApplication.class, args);
+	}
+
+}
